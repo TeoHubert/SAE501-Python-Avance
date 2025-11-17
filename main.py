c9df7b113a1b00ad7dc67a0c3070556ccc36b815
TeoHubert <teohubert00@gmail.com>
2025-11-17 14:59:31 +0100

    Create MainInTheMiddle class

---
+++ main.py
@@ -0,0 +1,12 @@
+class MainInTheMiddle:
+    def __init__(self, cible_1_ip, cible_2_ip, interface):
+        self.cible_1_ip = cible_1_ip
+        self.cible_2_ip = cible_2_ip
+        self.cible_1_mac = "" #Créer une instance de MacAddress
+        self.cible_2_mac = "" #Créer une instance de MacAddress
+        self.interface = interface
+
+    def start_mitm(self):
+        pass
+
+
