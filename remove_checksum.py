d25cdd255e2a2de92e7a012c9e89492857a37305
KillianDuchene <Killian.duchene@gmail.com>
2025-11-17 17:01:07 +0100

    Add script to remove checksum from packets
    Introduces remove_checksum.py, which provides a function to delete the checksum field from a given packet using Scapy. Includes example usage in commented code.

---
+++ remove_checksum.py
@@ -0,0 +1,11 @@
+from scapy.all import IP, TCP
+
+def remove_checksum(packet):
+    print(f"check sum du packet : {IP(bytes(packet)).chksum}")
+    del packet.chksum 
+    return packet
+
+
+#p = IP(dst="10.41.31.254")/TCP()
+
+#print(remove_checksum(IP(bytes(p))).chksum)
