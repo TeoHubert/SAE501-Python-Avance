d298fea51464730f13a67fe17330b2bc46a6206d
TeoHubert <teohubert00@gmail.com>
2025-11-20 10:54:47 +0100

    Améliorer la gestion des erreurs dans la fonction remove_checksum et réorganiser le code pour une meilleure lisibilité

--- remove_checksum.py
+++ remove_checksum.py
@@ -1,16 +1,19 @@
 from scapy.all import IP, TCP, UDP
 
 def remove_checksum(packet):
-    # print(f"check sum du packet : {IP(bytes(packet)).chksum}")
-    if TCP in packet:
-        del packet[TCP].chksum
-    elif IP in packet:
-        del packet[IP].chksum
-    elif UDP in packet:
-        del packet[UDP].chksum
-    else:
-        del packet.chksum 
-    return packet
+    try:
+        if TCP in packet:
+            del packet[TCP].chksum
+        elif IP in packet:
+            del packet[IP].chksum
+        elif UDP in packet:
+            del packet[UDP].chksum
+        else:
+            del packet.chksum
+        return packet 
+    except Exception as e:
+        print(f"Error removing checksum: {e}")
+        return packet
 
 
 #p = IP(dst="10.41.31.254")/TCP()
